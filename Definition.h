4507445aaa418e0345791f4998086e9bc900ed51
Wujh1995 <837538041@qq.com>
2016-05-29 00:54:37 +0800

    Add files via upload

---
+++ Definition.h
@@ -0,0 +1,17 @@
+// ball speed direction
+`define RIGHT 1'b1
+`define LEFT  1'b0
+`define UP    1'b0
+`define DOWN  1'b1
+
+// 7 seg LED definition
+`define ZERO 7'b1000000
+`define ONE 7'b1111001 
+`define TWO 7'b0100100 
+`define THREE 7'b0110000
+`define FOUR 7'b0011001 
+`define FIVE 7'b0010010 
+`define SIX 7'b0000010 
+`define SEVEN 7'b1111000  
+`define EIGHT 7'b0000000  
+`define NINE 7'b0010000  
